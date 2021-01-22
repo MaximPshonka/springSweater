7d77efb93a058a83f6ed702fcf114a8ef6c59a7e
Maxim Shchemelev <71179422+MaximPshonka@users.noreply.github.com>
2021-01-22 21:15:35 +0300

    dot com

---
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -0,0 +1,71 @@
+package com.example.sweater.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Message {
+    public Message(String text, String tag, User user) {
+        this.text = text;
+        this.tag = tag;
+        this.author = user;
+    }
+
+    public Message() {
+    }
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
+    private String text;
+    private String tag;
+
+    @ManyToOne(fetch = FetchType.EAGER) // одному User будет соответствовать множество Message
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public String getAuthorName(){
+        return author != null ? author.getUsername() : "null";
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
+
+
+/*
+spring.jpa.hibernate.ddl-auto=create-drop
+spring.jpa.show-sql=true
+spring.datasource.driver-class-name=org.postgresql.Driver
+spring.jpa.database=postgresql
+spring.jpa.database-platform=org.hibernate.dialect.PostgreSQLDialect
+*/
